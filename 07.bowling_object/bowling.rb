310ca83fb145174d4bf82292b2d760c0dc499601
tksmasaki <tk.masaki70@gmail.com>
2021-10-18 11:40:33 +0900

    テストの追加とリファクタリング

--- 07.bowling_object/bowling.rb
+++ 07.bowling_object/bowling.rb
@@ -2,28 +2,39 @@
 
 # frozen_string_literal: true
 
-scores = []
-ARGV[0].split(',').each do |score|
-  if score == 'X'
-    scores << 10 << 0
-  else
-    scores << score.to_i
+def main
+  puts calc_score(ARGV[0])
+end
+
+def calc_score(frames_str)
+  frames = frames_str_to_array(frames_str)
+  result = 0
+  frames.each_with_index do |frame, i|
+    result += frame.sum
+    next unless frame.sum == 10 && i < 9
+
+    result +=
+      if frame[0] == 10
+        frames[i + 1][0] == 10 ? frames[i + 1][0] + frames[i + 2][0] : frames[i + 1].sum
+      else
+        frames[i + 1][0]
+      end
   end
+
+  result
 end
 
-frames = scores.each_slice(2).to_a
-
-result = 0
-frames.each_with_index do |frame, i|
-  result += frame.sum
-  next unless frame.sum == 10 && i < 9
-
-  result +=
-    if frame[0] == 10
-      frames[i + 1][0] == 10 ? frames[i + 1][0] + frames[i + 2][0] : frames[i + 1].sum
+def frames_str_to_array(str)
+  scores = []
+  str.split(',').each do |score|
+    if score == 'X'
+      scores << 10 << 0
     else
-      frames[i + 1][0]
+      scores << score.to_i
     end
+  end
+
+  scores.each_slice(2).to_a
 end
 
-puts result
+main if __FILE__ == $PROGRAM_NAME
---
+++ 07.bowling_object/bowling_test.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'test/unit'
+require_relative './bowling'
+
+class BowlingTest < Test::Unit::TestCase
+  data(case1: [139, '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'],
+       case2: [164, '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X'],
+       case3: [107, '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4'],
+       case4: [134, '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0'],
+       case5: [300, 'X,X,X,X,X,X,X,X,X,X,X,X'])
+  def test_calc_score(data)
+    expected, target = data
+    assert_equal expected, calc_score(target)
+  end
+end
