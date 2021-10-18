80ec0e702c23484cb9f6ea0cdb5e669ef4c4c2cb
tksmasaki <tk.masaki70@gmail.com>
2021-10-18 10:18:36 +0900

    cp 04.bowling/bowling.rb 07.bowling_object

---
+++ 07.bowling_object/bowling.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+scores = []
+ARGV[0].split(',').each do |score|
+  if score == 'X'
+    scores << 10 << 0
+  else
+    scores << score.to_i
+  end
+end
+
+frames = scores.each_slice(2).to_a
+
+result = 0
+frames.each_with_index do |frame, i|
+  result += frame.sum
+  next unless frame.sum == 10 && i < 9
+
+  result +=
+    if frame[0] == 10
+      frames[i + 1][0] == 10 ? frames[i + 1][0] + frames[i + 2][0] : frames[i + 1].sum
+    else
+      frames[i + 1][0]
+    end
+end
+
+puts result
