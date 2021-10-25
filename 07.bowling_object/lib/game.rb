fb1739bdf0619543e5c2325ad068f2a5d04dbf66
tksmasaki <tk.masaki70@gmail.com>
2021-10-25 15:07:10 +0900

    Gameクラスをリファクタリング

--- 07.bowling_object/lib/game.rb
+++ 07.bowling_object/lib/game.rb
@@ -8,15 +8,12 @@ class Game
   end
 
   def score
-    result = 0
-    @frames.each_with_index do |frame, i|
-      result += frame.score
-      next unless frame.score == 10 && i < 9
-
-      result += add_score(i)
+    scores = @frames.map.with_index do |frame, i|
+      score = frame.score
+      score == 10 && i < 9 ? score + add_score(i) : score
     end
 
-    result
+    scores.sum
   end
 
   private
@@ -38,32 +35,24 @@ class Game
     frames = Array.new(9)
     marks = marks.each_slice(2).to_a
     marks.each_with_index do |frame_marks, i|
-      if i < 9
-        frames[i] = Frame.new(*frame_marks)
-      else
-        tenth_marks = []
-        marks[9..-1].each do |marks_array|
-          if marks_array[0] == 'X'
-            tenth_marks << 'X'
-          else
-            tenth_marks += marks_array
-          end
-        end
-        frames[9] = Frame.new(*tenth_marks)
-        break
-      end
+      break if i >= 9
+
+      frames[i] = Frame.new(*frame_marks)
     end
 
-    frames
+    tenth_marks = marks[9..].map { |v| v[0] == 'X' ? 'X' : v }
+    frames << Frame.new(*tenth_marks.flatten)
   end
 
   def add_score(index)
-    if @frames[index].marks[0] == 'X' && index == 8
+    frames = @frames[index..(index + 2)]
+
+    if frames[0].marks[0] == 'X' && index == 8
       @frames[9].shot_scores[0..1].sum
-    elsif @frames[index].marks[0] == 'X'
-      @frames[index + 1].marks[0] == 'X' ? @frames[index + 1].shot_scores[0] + @frames[index + 2].shot_scores[0] : @frames[index + 1].score
+    elsif frames[0].marks[0] == 'X'
+      frames[1].marks[0] == 'X' ? frames[1].shot_scores[0] + frames[2].shot_scores[0] : frames[1].score
     else
-      @frames[index + 1].shot_scores[0]
+      frames[1].shot_scores[0]
     end
   end
 end
