25ad17a44ba7d58b54541ea413be5fab02bfc1cb
tksmasaki <tk.masaki70@gmail.com>
2021-10-27 15:20:57 +0900

    LsFileクラスのリファクタリング

--- 08.ls_object/lib/ls_file.rb
+++ 08.ls_object/lib/ls_file.rb
@@ -4,18 +4,40 @@ require 'etc'
 require_relative './mode_converter'
 
 class LsFile
-  attr_reader :name, :mode, :nlink, :owner, :group, :size, :mtime, :blocks
-
   def initialize(file_path)
-    @name = File.basename(file_path)
-    @size = File.size(file_path)
-    @mtime = File.mtime(file_path)
+    @file_path = file_path
+    @stat = File.stat(file_path)
+  end
 
-    stat = File.stat(file_path)
-    @mode = ModeConverter.convert_file_mode(stat.mode)
-    @owner = Etc.getpwuid(stat.uid).name
-    @group = Etc.getgrgid(stat.gid).name
-    @nlink = stat.nlink
-    @blocks = stat.blocks
+  def name
+    File.basename(@file_path)
+  end
+
+  def size
+    File.size(@file_path)
+  end
+
+  def mtime
+    File.mtime(@file_path)
+  end
+
+  def mode
+    ModeConverter.convert_file_mode(@stat.mode)
+  end
+
+  def owner
+    Etc.getpwuid(@stat.uid).name
+  end
+
+  def group
+    Etc.getgrgid(@stat.gid).name
+  end
+
+  def nlink
+    @stat.nlink
+  end
+
+  def blocks
+    @stat.blocks
   end
 end
