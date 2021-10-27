e20b05da92d4600a80a0842c5e38d425cab8d16e
tksmasaki <tk.masaki70@gmail.com>
2021-10-27 15:17:41 +0900

    ModeConverterクラスのリファクタリング

--- 08.ls_object/lib/mode_converter.rb
+++ 08.ls_object/lib/mode_converter.rb
@@ -1,37 +1,31 @@
 # frozen_string_literal: true
 
 class ModeConverter
-  class << self
-    def convert_file_mode(mode)
-      octal_mode = format('%06d', mode.to_s(8))
-      convert_octal_type(octal_mode[0..1]) + octal_mode[3..5].chars.map { |v| convert_octal_permission(v) }.join
-    end
+  FILE_TYPES = {
+    '01' => 'p',
+    '02' => 'c',
+    '04' => 'd',
+    '06' => 'b',
+    '10' => '-',
+    '12' => 'l',
+    '14' => 's'
+  }.each_value(&:freeze).freeze
 
-    private
+  FILE_PERMISSIONS = {
+    '0' => '---',
+    '1' => '--x',
+    '2' => '-w-',
+    '3' => '-wx',
+    '4' => 'r--',
+    '5' => 'r-x',
+    '6' => 'rw-',
+    '7' => 'rwx'
+  }.each_value(&:freeze).freeze
 
-    def convert_octal_type(type_num)
-      {
-        '01' => 'p',
-        '02' => 'c',
-        '04' => 'd',
-        '06' => 'b',
-        '10' => '-',
-        '12' => 'l',
-        '14' => 's'
-      }[type_num]
-    end
-
-    def convert_octal_permission(permission_num)
-      {
-        '0' => '---',
-        '1' => '--x',
-        '2' => '-w-',
-        '3' => '-wx',
-        '4' => 'r--',
-        '5' => 'r-x',
-        '6' => 'rw-',
-        '7' => 'rwx'
-      }[permission_num]
-    end
+  def self.convert_file_mode(mode)
+    octal_mode = format('%06d', mode.to_s(8))
+    type = FILE_TYPES[octal_mode[0..1]]
+    permission = octal_mode[3..5].chars.map { |c| FILE_PERMISSIONS[c] }.join
+    type + permission
   end
 end
